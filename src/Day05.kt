04c999e2eb67aa04bd8cca4b30515cc0fe960c79
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2023-12-06 12:41:34 +0100

    Day05 - Second Part + refactor

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,51 +1,57 @@
 import kotlin.io.path.Path
 import kotlin.io.path.readText
-
-
-data class Node(val key: LongRange, val destinationRange: LongRange, var left: Node? = null, var right: Node? = null) {
-    fun insert(value: LongRange, destinationRange: LongRange) {
-        if (value.first > key.first) {
-            if (right == null) right = Node(value, destinationRange) else right!!.insert(value, destinationRange)
-        } else {
-            if (left == null) left = Node(value, destinationRange) else left!!.insert(value, destinationRange)
-        }
-    }
-
-    fun find(value: Long): Long? {
-        if (key.contains(value)) {
-            val distance = value - key.first
-            return destinationRange.first + distance
-        }
-        return if (value > key.first) {
-            if (right == null) null else right!!.find(value)
-        } else {
-            if (left == null) null else left!!.find(value)
-        }
+data class MapValue(val destinationRange: LongRange, val sourceRange: LongRange) {
+    fun sourceToDest(source: Long): Long {
+        if (!sourceRange.contains(source)) error("$sourceRange does not contains $source")
+        return destinationRange.first + (source - sourceRange.first)
     }
 }
 
-data class Map(val tree: Node)
-
-data class MapValue(val destinationRange: LongRange, val sourceRange: LongRange)
-
-fun parseMap(raw: String): Node? {
-    var node: Node? = null
-
-    raw
+fun parseMap(raw: String): List<MapValue> {
+    return raw
         .lines()
         .drop(1)
         .filter { it.isNotEmpty() }
-        .map {
-        val (destination, source, range) = it.split(" ").map { s -> s.toLong() }
-        MapValue(destination.rangeTo(destination + range - 1), source.rangeTo(source + range - 1))
-    }.forEach {
-        if (node == null) {
-            node = Node(it.sourceRange, it.destinationRange)
-        } else {
-            node!!.insert(it.sourceRange, it.destinationRange)
+        .map {s ->
+            val (destination, source, range) = s.split(" ").map { it.toLong() }
+            MapValue(destination..<destination + range, source..<source + range)
+        }.sortedBy { it.sourceRange.first }
+}
+
+fun getDestinationFromSource(map: List<MapValue>, source: Long): Long {
+    map.find { it.sourceRange.contains(source) }.let {
+        if (it == null) return source
+        return it.sourceToDest(source)
+    }
+}
+fun getDestinationFromSource(map: List<MapValue>, source: LongRange): List<LongRange> {
+    val result = mutableListOf<LongRange>()
+    var rest = source
+
+    for (it in map) {
+        // fully contained
+        if (it.sourceRange.contains(rest.first) && it.sourceRange.contains(rest.last)) {
+            result.add(it.sourceToDest(rest.first)..it.sourceToDest(rest.last))
+            rest = LongRange.EMPTY
+            break
+        } else if (rest.last < it.sourceRange.first) {
+            break
+        } else if (it.sourceRange.contains(rest.last) && !it.sourceRange.contains(rest.first)) {
+            result.add(rest.first..<it.sourceRange.first)
+            result.add(it.sourceToDest(it.sourceRange.first)..it.sourceToDest(rest.last))
+            rest = LongRange.EMPTY
+            break
+        } else if (it.sourceRange.contains(rest.first) && !it.sourceRange.contains(rest.last)) {
+            result.add(it.sourceToDest(rest.first)..it.sourceToDest(it.sourceRange.last))
+            rest = it.sourceRange.last + 1 .. rest.last
+        } else if (rest.first < it.sourceRange.first && rest.last > it.sourceRange.last) {
+            result.add(rest.first..<it.sourceRange.first)
+            result.add(it.sourceToDest(it.sourceRange.first)..it.sourceToDest(it.sourceRange.last))
+            rest = it.sourceRange.last + 1 .. rest.last
         }
     }
-    return node
+    if (!rest.isEmpty()) result.add(rest)
+    return result
 }
 
 fun main() {
@@ -53,29 +59,38 @@ fun main() {
         val mapsRaw = Path(filePath).readText().split("\n\n")
         val rest = mapsRaw.drop(1)
         var seedsIds = mapsRaw.first().drop(7).split(" ").map { it.toLong() }
-        val maps = rest.filter { it.isNotEmpty() }.map { parseMap(it) }
+        val maps = rest.map { parseMap(it) }
 
-        maps.forEach {node ->
+        maps.forEach {map ->
             seedsIds = seedsIds.map {
-                node?.find(it) ?: it
+                getDestinationFromSource(map, it)
             }
         }
 
         return seedsIds.min()
     }
 
+    fun part2(filePath: String): Long {
+        val mapsRaw = Path(filePath).readText().split("\n\n")
+        val rest = mapsRaw.drop(1)
+        var seedsIds = mapsRaw.first().drop(7).split(" ").map { it.toLong() }.chunked(2).map {
+                (first, second) -> first..<second + first
+        }
 
+        val maps = rest.map { parseMap(it) }
 
-
-
-    fun part2(input: List<String>): Int {
-        return 0
+        maps.forEach {map ->
+            seedsIds = seedsIds.flatMap {
+                getDestinationFromSource(map, it)
+            }
+        }
+        return seedsIds.minOf { longRange: LongRange -> longRange.first }
     }
 
     // test if implementation meets criteria from the description, like:
     check(part1("src/Day05_test.txt") == 35L)
-//    check(part2(testInput) == 0)
-//    val input = readInput("Day05")
+    part2("src/Day05_test.txt").println()
+    check(part2("src/Day05_test.txt") == 46L)
     part1("src/Day05.txt").println()
-//    part2(input).println()
+    part2("src/Day05.txt").println()
 }
